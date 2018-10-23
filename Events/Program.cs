a0290e6553a2e52f9fdedf99cef8c7c8a5367cbd
fabian77 <fabian77@web.de>
2018-10-23 10:05:59 +0200

    Old way of using events

---
+++ Events/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Adder();
+            //a.OnMultipleOfFiveReached += new Adder.dgEventRaiser(a_MultipleOfFiveReached);
+            a.OnMultipleOfFiveReached += a_MultipleOfFiveReached;
+
+            int iAnswer = a.Add(4, 3);
+            Console.WriteLine("iAnswer = {0}", iAnswer);
+            iAnswer = a.Add(4, 6);
+            Console.WriteLine("iAnswer = {0}", iAnswer);
+            Console.ReadKey();
+        }
+
+        static void a_MultipleOfFiveReached()
+        {
+            Console.WriteLine("Multiple of five reached!");
+        }
+    }
+
+    public class Adder
+    {
+        public delegate void dgEventRaiser();
+        public event dgEventRaiser OnMultipleOfFiveReached;
+
+        public int Add(int x, int y)
+        {
+            int iSum = x + y;
+            if ((iSum % 5 == 0) && (OnMultipleOfFiveReached != null))
+            {
+                OnMultipleOfFiveReached();
+            }
+            return iSum;
+        }
+    }
+}
